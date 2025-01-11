9c963bd35157d59e68102cf123aecaceff8d2485
rhuanpk <dev@rhuanpk.net>
2025-01-11 01:32:23 -0300

    feat: add example of how to process video in parallel

--- main.go
+++ main.go
@@ -18,10 +18,21 @@ const (
 	frameHighQuality = 1
 
 	videoPath     = "video/sample.mp4"
-	frameInterval = 1
+	frameInterval = 5
 )
 
+func formatDuration(seconds int) string {
+	duration := time.Duration(seconds) * time.Second
+	hours := int(duration.Hours())
+	minutes := int(duration.Minutes()) % 60
+	secs := int(duration.Seconds()) % 60
+
+	return fmt.Sprintf("%02d:%02d:%02d", hours, minutes, secs)
+}
+
 func extractFrames(videoPath, outputDir string, frameInterval int, highQuality bool) error {
+	// errChan := make(chan error, 1)
+
 	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
 		return err
 	}
@@ -30,6 +41,56 @@ func extractFrames(videoPath, outputDir string, frameInterval int, highQuality b
 	if highQuality {
 		kwArgs["q:v"] = frameHighQuality
 	}
+
+	// videoProbe, err := ffmpeg.Probe(videoPath)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// // // videoSize, err := strconv.ParseInt(gjson.Get(videoProbe, "format.size").Str, 0, 64)
+	// // videoSize, err := strconv.ParseFloat(gjson.Get(videoProbe, "format.size").Str, 64)
+	// // if err != nil {
+	// // 	return err
+	// // }
+	// // videoSizeMB := videoSize / (1 << 20)
+
+	// videoDuration, err := strconv.ParseFloat(gjson.Get(videoProbe, "format.duration").Str, 64)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// frameIntervalStep := frameInterval * 3
+	// if frameIntervalStep >= 10 {
+	// 	frameIntervalStep = frameIntervalStep / 2
+	// }
+	// goRoutinesCount := int(videoDuration) / frameIntervalStep
+
+	// var wg sync.WaitGroup
+	// for index := 0; index < goRoutinesCount; index++ {
+	// 	wg.Add(1)
+	// 	go func(index int, kwArgs ffmpeg.KwArgs) {
+	// 		defer wg.Done()
+	// 		startInterval := frameIntervalStep * index
+	// 		kwArgs["ss"] = formatDuration(startInterval)
+	// 		kwArgs["to"] = formatDuration(frameIntervalStep * (index + 1))
+	// 		kwArgs["start_number"] = startInterval/frameInterval + 1
+	// 		err := ffmpeg.Input(videoPath).
+	// 			Output(filepath.Join(outputDir, strings.Replace(frameName, "*", "%04d", 1)), kwArgs).
+	// 			// OverWriteOutput().RunWithResource(0.75, 1.0)
+	// 			OverWriteOutput().Run()
+	// 		if err != nil {
+	// 			errChan <- err
+	// 		}
+	// 	}(index, kwArgs)
+	// }
+
+	// go func() {
+	// 	wg.Wait()
+	// 	close(errChan)
+	// }()
+	// if err, ok := <-errChan; ok {
+	// 	return err
+	// }
 
 	err := ffmpeg.Input(videoPath).
 		Output(filepath.Join(outputDir, strings.Replace(frameName, "*", "%04d", 1)), kwArgs).
