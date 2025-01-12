1bfe3e9df4e096ab161f6c09092534ec64d07443
rhuanpk <dev@rhuanpk.net>
2025-01-11 21:14:46 -0300

    feat: add multi video processing

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	ffmpeg "github.com/u2takey/ffmpeg-go"
@@ -21,6 +23,8 @@ const (
 	frameInterval = 5
 )
 
+var videosPaths = []string{"video/sample_1.mp4", "video/sample_2.mp4"}
+
 func formatDuration(seconds int) string {
 	duration := time.Duration(seconds) * time.Second
 	hours := int(duration.Hours())
@@ -30,9 +34,13 @@ func formatDuration(seconds int) string {
 	return fmt.Sprintf("%02d:%02d:%02d", hours, minutes, secs)
 }
 
-func extractFrames(videoPath, outputDir string, frameInterval int, highQuality bool) error {
-	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-		return err
+func extractFrames(videosPaths, outputsDirs []string, frameInterval int, highQuality bool) error {
+	errChan := make(chan error, 1)
+
+	for _, outputDir := range outputsDirs {
+		if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+			return err
+		}
 	}
 
 	kwArgs := ffmpeg.KwArgs{"vf": fmt.Sprintf("fps=1/%d", frameInterval)}
@@ -40,11 +48,31 @@ func extractFrames(videoPath, outputDir string, frameInterval int, highQuality b
 		kwArgs["q:v"] = frameHighQuality
 	}
 
-	err := ffmpeg.Input(videoPath).
-		Output(filepath.Join(outputDir, strings.Replace(frameName, "*", "%04d", 1)), kwArgs).
-		// OverWriteOutput().RunWithResource(0.75, 1.0)
-		OverWriteOutput().Run()
-	if err != nil {
+	var wg sync.WaitGroup
+	for index, videoPath := range videosPaths {
+		wg.Add(1)
+		go func(index int, videoPath string) {
+			defer wg.Done()
+			err := ffmpeg.Input(videoPath).
+				Output(filepath.Join(
+					outputsDirs[index],
+					strings.Replace(frameName, "*", "%04d", 1)),
+					kwArgs,
+				).
+				// OverWriteOutput().RunWithResource(0.75, 1.0)
+				OverWriteOutput().Run()
+			if err != nil {
+				errChan <- err
+			}
+		}(index, videoPath)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	if err, ok := <-errChan; ok {
 		return err
 	}
 
@@ -71,57 +99,80 @@ func addFileToZip(zipWriter *zip.Writer, filePath string) error {
 	return nil
 }
 
-func createZip(zipPath, sourceDir string) error {
-	zipFile, err := os.Create(zipPath)
-	if err != nil {
-		return err
-	}
-	defer zipFile.Close()
-
-	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
-
-	frames, err := filepath.Glob(filepath.Join(sourceDir, frameName))
-	if err != nil {
-		return err
-	}
-
-	for _, frame := range frames {
-		if err := addFileToZip(zipWriter, frame); err != nil {
+func createZip(zipsPaths, sourcesDirs []string) error {
+	for index, zipPath := range zipsPaths {
+		zipFile, err := os.Create(zipPath)
+		if err != nil {
 			return err
+		}
+		defer zipFile.Close()
+
+		zipWriter := zip.NewWriter(zipFile)
+		defer zipWriter.Close()
+
+		frames, err := filepath.Glob(filepath.Join(sourcesDirs[index], frameName))
+		if err != nil {
+			return err
+		}
+
+		for _, frame := range frames {
+			if err := addFileToZip(zipWriter, frame); err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
 }
 
-func processVideo(videoPath string, frameInterval int, highQuality bool) (string, error) {
-	videoDir := filepath.Dir(videoPath)
+func processVideo(videosPaths []string, frameInterval int, highQuality bool) ([]string, error) {
+	var videosDirs, framesDirs, videosNames, videosNamesWithoutExt, zipsPaths []string
 
-	framesDir := filepath.Join(videoDir, "frames")
-	if err := os.MkdirAll(framesDir, os.ModePerm); err != nil {
-		return "", err
+	for _, videoPath := range videosPaths {
+		videosDirs = append(videosDirs, filepath.Dir(videoPath))
+	}
+
+	for index, videoPath := range videosPaths {
+		videoName := filepath.Base(videoPath)
+		videosNames = append(videosNames, videoName)
+
+		videoNameWithoutExt := strings.TrimSuffix(videoName, filepath.Ext(videoName))
+		videosNamesWithoutExt = append(videosNamesWithoutExt, videoNameWithoutExt)
+
+		unixTime := strconv.Itoa(int(time.Now().UnixNano()))
+		zipName := videoNameWithoutExt + "_frames_" + unixTime + ".zip"
+		zipsPaths = append(zipsPaths, filepath.Join(videosDirs[index], zipName))
+	}
+
+	for index, videoDir := range videosDirs {
+		framesDir, err := os.MkdirTemp(videoDir, videosNamesWithoutExt[index]+"_frames_")
+		if err != nil {
+			return nil, err
+		}
+
+		if err := os.MkdirAll(framesDir, os.ModePerm); err != nil {
+			return nil, err
+		}
+		framesDirs = append(framesDirs, framesDir)
 	}
 
 	defer func() {
-		if err := os.RemoveAll(framesDir); err != nil {
-			log.Println("error in remove frames dir:", err)
+		for _, framesDir := range framesDirs {
+			if err := os.RemoveAll(framesDir); err != nil {
+				log.Println("error in remove frames dir:", err)
+			}
 		}
 	}()
 
-	videoName := filepath.Base(videoPath)
-	zipName := strings.TrimSuffix(videoName, filepath.Ext(videoName)) + "_frames.zip"
-	zipPath := filepath.Join(videoDir, zipName)
-
-	if err := extractFrames(videoPath, framesDir, frameInterval, highQuality); err != nil {
-		return "", err
+	if err := extractFrames(videosPaths, framesDirs, frameInterval, highQuality); err != nil {
+		return nil, err
 	}
 
-	if err := createZip(zipPath, framesDir); err != nil {
-		return "", err
+	if err := createZip(zipsPaths, framesDirs); err != nil {
+		return nil, err
 	}
 
-	return zipPath, nil
+	return zipsPaths, nil
 }
 
 func main() {
@@ -129,11 +180,12 @@ func main() {
 		log.Println("execution time:", time.Since(start).String())
 	}(time.Now())
 
-	zipPath, err := processVideo(videoPath, frameInterval, true)
+	// zipsPaths, err := processVideo([]string{videoPath}, frameInterval, true)
+	zipsPaths, err := processVideo(videosPaths, frameInterval, true)
 	if err != nil {
-		log.Println("error in process video:", err)
+		log.Println("error in process videos:", err)
 		return
 	}
 
-	log.Println("success in process video:", zipPath)
+	log.Println("success in process videos:", zipsPaths)
 }
